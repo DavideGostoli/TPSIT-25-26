c245292b6dbb984461445b5f249fc59d98f620eb
DavideGostoli <145323257+DavideGostoli@users.noreply.github.com>
2025-12-03 22:21:06 +0100

    Update script.js

--- anagrafica 2.0/script.js
+++ anagrafica 2.0/script.js
@@ -1,18 +1,18 @@
 
 var x = 0;
 
-var stampa = "";
-var stampa2 = "";
-var stampa3 = "";
-var stampa4 = "";
+var righePrincipali = "";
+var righeEta = "";
+var righeLettera = "";
+var righeGenerazioni = "";
 
-var xmlhttp = new XMLHttpRequest();
+var richiesta = new richiestaRequest();
 
-xmlhttp.open("GET", "elenco.xml", true);
+richiesta.open("GET", "elenco.xml", true);
 
-xmlhttp.send();
+richiesta.send();
 
-xmlhttp.onreadystatechange = function () {
+richiesta.onreadystatechange = function () {
 
     if (this.readyState == 4 && this.status == 200) {
 
@@ -20,40 +20,40 @@ xmlhttp.onreadystatechange = function () {
         var persone = xmlDoc.getElementsByTagName("persona");
 
         for (x = 0; x < persone.length; x++) {
-            var nome = persone[x].getElementsByTagName("nome")[0].chilDNodes[0].nodeValue;
-            var cognome = persone[x].getElementsByTagName("cognome")[0].chilDNodes[0].nodeValue;
-            var anni = persone[x].getElementsByTagName("anni")[0].chilDNodes[0].nodeValue;
-            var DN = persone[x].getElementsByTagName("DN")[0].chilDNodes[0].nodeValue;
+            var nome = persone[x].getElementsByTagName("nome")[0].chilnascitaodes[0].nodeValue;
+            var cognome = persone[x].getElementsByTagName("cognome")[0].chilnascitaodes[0].nodeValue;
+            var anni = persone[x].getElementsByTagName("anni")[0].chilnascitaodes[0].nodeValue;
+            var nascita = persone[x].getElementsByTagName("nascita")[0].chilnascitaodes[0].nodeValue;
             
-            stampa += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${DN} </td></tr>`;
+            righePrincipali += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${nascita} </td></tr>`;
         }
 
-        document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di DN </th></tr> ${stampa}`;
-        stampa = "";
+        document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di nascita </th></tr> ${righePrincipali}`;
+        righePrincipali = "";
 
     }
 
 };
 
 function inizio(){
-    xmlhttp.open("GET", "elenco.xml", true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function () {
+    richiesta.open("GET", "elenco.xml", true);
+    richiesta.send();
+    richiesta.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var xmlDoc = this.responseXML;
             var persone = xmlDoc.getElementsByTagName("persona");
 
             for (x = 0; x < persone.length; x++) {
-                var nome = persone[x].getElementsByTagName("nome")[0].chilDNodes[0].nodeValue;
-                var cognome = persone[x].getElementsByTagName("cognome")[0].chilDNodes[0].nodeValue;
-                var anni = persone[x].getElementsByTagName("anni")[0].chilDNodes[0].nodeValue;
-                var DN = persone[x].getElementsByTagName("DN")[0].chilDNodes[0].nodeValue;
+                var nome = persone[x].getElementsByTagName("nome")[0].chilnascitaodes[0].nodeValue;
+                var cognome = persone[x].getElementsByTagName("cognome")[0].chilnascitaodes[0].nodeValue;
+                var anni = persone[x].getElementsByTagName("anni")[0].chilnascitaodes[0].nodeValue;
+                var nascita = persone[x].getElementsByTagName("nascita")[0].chilnascitaodes[0].nodeValue;
                 
-                stampa += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${DN} </td></tr>`;
+                righePrincipali += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${nascita} </td></tr>`;
             }
             
-            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di DN </th></tr> ${stampa}`;
-            stampa = "";
+            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di nascita </th></tr> ${righePrincipali}`;
+            righePrincipali = "";
         }
     };
 }
@@ -61,24 +61,24 @@ function inizio(){
 function ricercaEta() {
     var minEta = document.getElementById("anni").value;
     document.getElementById("anni").value = "";
-    xmlhttp.open("GET", "elenco.xml", true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function () {
+    richiesta.open("GET", "elenco.xml", true);
+    richiesta.send();
+    richiesta.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var xmlDoc = this.responseXML;
             var persone = xmlDoc.getElementsByTagName("persona");
 
             for (x = 0; x < persone.length; x++) {
-                var cognome = persone[x].getElementsByTagName("cognome")[0].chilDNodes[0].nodeValue;
-                var anni = persone[x].getElementsByTagName("anni")[0].chilDNodes[0].nodeValue;
+                var cognome = persone[x].getElementsByTagName("cognome")[0].chilnascitaodes[0].nodeValue;
+                var anni = persone[x].getElementsByTagName("anni")[0].chilnascitaodes[0].nodeValue;
                 
                 if (anni >= minEta) {
-                    stampa2 += `<tr><td> ${cognome} </td><td> ${anni} </td></tr>`;
+                    righeEta += `<tr><td> ${cognome} </td><td> ${anni} </td></tr>`;
                 }
             }
             
-            document.getElementById("demo").innerHTML = `<tr><th> cognome </th><th> anni </th></tr> ${stampa2}`;
-            stampa2 = "";
+            document.getElementById("demo").innerHTML = `<tr><th> cognome </th><th> anni </th></tr> ${righeEta}`;
+            righeEta = "";
         }
     };
 }
@@ -91,67 +91,68 @@ function ricercaIniziale() {
         var iniziale = document.getElementById("iniziale").value.charAt(0).toUpperCase();
         document.getElementById("iniziale").value = "";
     }
-    xmlhttp.open("GET", "elenco.xml", true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function () {
+    richiesta.open("GET", "elenco.xml", true);
+    richiesta.send();
+    richiesta.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var xmlDoc = this.responseXML;
             var persone = xmlDoc.getElementsByTagName("persona");
 
             for (x = 0; x < persone.length; x++) {
-                var nome = persone[x].getElementsByTagName("nome")[0].chilDNodes[0].nodeValue;
-                var cognome = persone[x].getElementsByTagName("cognome")[0].chilDNodes[0].nodeValue;
-                var anni = persone[x].getElementsByTagName("anni")[0].chilDNodes[0].nodeValue;
-                var DN = persone[x].getElementsByTagName("DN")[0].chilDNodes[0].nodeValue;
+                var nome = persone[x].getElementsByTagName("nome")[0].chilnascitaodes[0].nodeValue;
+                var cognome = persone[x].getElementsByTagName("cognome")[0].chilnascitaodes[0].nodeValue;
+                var anni = persone[x].getElementsByTagName("anni")[0].chilnascitaodes[0].nodeValue;
+                var nascita = persone[x].getElementsByTagName("nascita")[0].chilnascitaodes[0].nodeValue;
                 
                 if (cognome.startsWith(iniziale)) {
-                    stampa3 += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${DN} </td></tr>`;
+                    righeLettera += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${nascita} </td></tr>`;
                 }
             }
             
-            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di DN </th></tr> ${stampa3}`;
-            stampa3 = "";
+            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di nascita </th></tr> ${righeLettera}`;
+            righeLettera = "";
         }   
     };
 }
 
 function generazione() {
     var iniziale = document.getElementById("iniziale").value;
-    xmlhttp.open("GET", "elenco.xml", true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange = function () {
+    richiesta.open("GET", "elenco.xml", true);
+    richiesta.send();
+    richiesta.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var xmlDoc = this.responseXML;
             var persone = xmlDoc.getElementsByTagName("persona");
 
             for (x = 0; x < persone.length; x++) {
-                var nome = persone[x].getElementsByTagName("nome")[0].chilDNodes[0].nodeValue;
-                var cognome = persone[x].getElementsByTagName("cognome")[0].chilDNodes[0].nodeValue;
-                var anni = persone[x].getElementsByTagName("anni")[0].chilDNodes[0].nodeValue;
-                var DN = persone[x].getElementsByTagName("DN")[0].chilDNodes[0].nodeValue;
+                var nome = persone[x].getElementsByTagName("nome")[0].chilnascitaodes[0].nodeValue;
+                var cognome = persone[x].getElementsByTagName("cognome")[0].chilnascitaodes[0].nodeValue;
+                var anni = persone[x].getElementsByTagName("anni")[0].chilnascitaodes[0].nodeValue;
+                var nascita = persone[x].getElementsByTagName("nascita")[0].chilnascitaodes[0].nodeValue;
                 
-                stampa4 += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${DN} </td>`;
+                righeGenerazioni += `<tr><td> ${nome} </td><td> ${cognome} </td><td> ${anni} </td><td> ${nascita} </td>`;
                 
-                if(DN >= 2013)
-                    stampa4 += `<td> Generazione Alpha </td></tr>`;
-                else if (DN >= 1997) {
-                    stampa4 += `<td> Generazione Z </td></tr>`;
-                } else if(DN >= 1981)
-                    stampa4 += `<td> Millenials </td></tr>`;
-                else if (DN >= 1965) {
-                    stampa4 += `<td> Generazione X </td></tr>`;
-                } else if(DN >= 1946)
-                    stampa4 += `<td> Baby Boomers </td></tr>`;
-                else if (DN >= 1928) {
-                    stampa4 += `<td> Generazione Silenziosa </td></tr>`;
+                if(nascita >= 2013)
+                    righeGenerazioni += `<td> Generazione Alpha </td></tr>`;
+                else if (nascita >= 1997) {
+                    righeGenerazioni += `<td> Generazione Z </td></tr>`;
+                } else if(nascita >= 1981)
+                    righeGenerazioni += `<td> Millenials </td></tr>`;
+                else if (nascita >= 1965) {
+                    righeGenerazioni += `<td> Generazione X </td></tr>`;
+                } else if(nascita >= 1946)
+                    righeGenerazioni += `<td> Baby Boomers </td></tr>`;
+                else if (nascita >= 1928) {
+                    righeGenerazioni += `<td> Generazione Silenziosa </td></tr>`;
                 } else
-                    stampa4 += `<td> Greatest Generation </td></tr>`;
+                    righeGenerazioni += `<td> Greatest Generation </td></tr>`;
             }
             
-            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di DN </th><th> generazione </th></tr> ${stampa4}`;
-            stampa4 = "";
+            document.getElementById("demo").innerHTML = `<tr><th> nome </th><th> cognome </th><th> anni </th><th> Data di nascita </th><th> generazione </th></tr> ${righeGenerazioni}`;
+            righeGenerazioni = "";
         }
     };
 }
+
 
 
